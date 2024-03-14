a134f744f4166c4eaa88dc895681539263255be5
宴之敖 <cmyrunrunrun@gmail.com>
2024-03-14 10:17:46 +0800

    tmpSave

--- data/dataPreProcess.py
+++ data/dataPreProcess.py
@@ -22,14 +22,17 @@ date_range = pd.date_range(start=start, end=end, freq='4h')
 
 # Reindex the DataFrame
 df = df.reindex(date_range)
+df = df[['o', 'h', 'l', 'c', 'v', 'qav', 'num_trades']]
 
 # Select only numeric columns for interpolation
+for col in df.columns:
+    df[col] = pd.to_numeric(df[col], errors='coerce')
+
 df = df.infer_objects()
 numeric_cols = df.select_dtypes(include=['number']).columns
 df[numeric_cols] = df[numeric_cols].interpolate(method='linear')
 
 # 选取的列: 开盘时间open_time，开盘o，最高h，最低l，收盘c，交易量v，qav交易的usdt数量，成交数num_trades
-df = df[['open_time', 'o', 'h', 'l', 'c', 'v', 'qav', 'num_trades']]
 path = './processedData/' + symbol
 fileProcessor.save_dataframe_to_pickle(df, path + '.pkl')
 fileProcessor.save_dataframe_to_csv(df, path + '.csv')
